8a98b202c2ec6e8e268fe3a94230cb36b6597e0b
Apiwat Sangakong <45027395+backbuu@users.noreply.github.com>
2020-02-04 11:14:03 +0700

    Update FizzBuzzApplication.java

--- src/main/java/com/example/FizzBuzz/FizzBuzzApplication.java
+++ src/main/java/com/example/FizzBuzz/FizzBuzzApplication.java
@@ -10,26 +10,9 @@ public class FizzBuzzApplication {
 		if(canDivideBy(number, 3)) return "Fizz";
 
 		return String.valueOf(number);
-		// if (isBuzz(number) && isFizz(number)) return "FizzBizz";
-		// if (isBuzz(number))	return "Bizz";
-		// if (isFizz(number)) return "Fizz";
 	}
 
 	private Boolean canDivideBy(int number, int devider){
 		return number % devider == 0;
 	}
-
-	// private Boolean isFizz(int number) {
-	// 	if(number % 3 == 0){
-	// 	return true;
-	// 	}
-	// 	return false;
-	// }
-	// private Boolean isBuzz(int number){
-	// 	if(number %5 ==0){
-	// 		return true;
-	// 	}
-	// 	return false;
-	// }
-
 }
